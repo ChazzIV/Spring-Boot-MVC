916176383d901b2f547f4a5cbfe2a0a3c13c5354
ChazzIV <chazz.iv@gmail.com>
2020-05-27 14:57:55 -0500

    Aplicacion en Spring boot MVC

---
+++ src/main/java/mx/com/gm/util/EncryptarPassword.java
@@ -0,0 +1,18 @@
+package mx.com.gm.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncryptarPassword {
+    public static void main(String[] args) {
+        
+        
+        var password = "123";
+        System.out.println("password: " + password);
+        System.out.println("password encruptado: " + encriptarPassword(password));
+    }
+    
+    public static String encriptarPassword(String password){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+}
